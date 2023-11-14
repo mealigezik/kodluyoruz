ff896bdf4b030bebcde6d7867ce22dafe8c69fdb
Ali Gezik <bilgepanter@gmail.com>
2023-11-14 22:40:35 +0100

    q

---
+++ javascript/lesson1/app2.js
@@ -0,0 +1,19 @@
+// console.log(document.head);
+// console.log(document.body);
+
+// document.write(document.head)
+// document.write(document.body)
+
+document.body.style.backgroundColor = "brown"
+
+
+// let h2 = document.getElementsByTagName('h2')
+
+// console.log(h2.title.innerHTML)
+
+let title = document.getElementById('title')
+console.log(title)
+console.log(title.innerHTML)
+console.log(title.innerText)
+console.log(typeof title.innerText)
+console.log(typeof(title.innerHTML))
