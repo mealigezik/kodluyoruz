66ae7704c720f54199440a77052a41f64e9167f9
Ali Gezik <bilgepanter@gmail.com>
2023-11-15 18:42:41 +0100

    q

--- javascript/lesson1/app3.js
+++ javascript/lesson1/app3.js
@@ -1,5 +1,25 @@
-let fullName = prompt("Please enter name: ")
+// veri girisi alma
 
-let greeting = document.querySelector("#greeting")
+// let fullName = prompt("Please enter name: ")
+// let greeting = document.querySelector("#greeting")
+// greeting.innerHTML = `${greeting.innerHTML}<hr><small style="color:red;">${fullName}</small>`
 
-greeting.innerHTML = `${greeting.innerHTML}<hr><br><br> <small style="color:red;">${fullName}</small>`
+let item = document.querySelector("ul#myList>li:last-child")
+console.log(item);
+
+let item2 = document.querySelector("ul#myList>li:first-child").innerHTML
+console.log(item2);
+
+document.querySelector("ul#myList>li:first-child").innerHTML = "Artik Bursa"
+
+let ulDOM = document.querySelector("ul#myList").innerText
+console.log(ulDOM);
+
+let liDOM = document.createElement("li")
+liDOM.innerHTML = "self created element"
+
+ulDOM.append(liDOM)
+
+console.log(item);
+
+// console.log(liDOM);
