f849f0f16e644d0ceedffb1902cf5eb426fd3745
Ali Gezik <bilgepanter@gmail.com>
2023-11-16 01:41:04 +0100

    q

--- javascript/lesson1/app3.js
+++ javascript/lesson1/app3.js
@@ -4,25 +4,39 @@
 // let greeting = document.querySelector("#greeting")
 // greeting.innerHTML = `${greeting.innerHTML}<hr><small style="color:red;">${fullName}</small>`
 
-let item = document.querySelector("ul#myList>li:last-child")
-console.log(item);
+// let item = document.querySelector("ul#myList>li:last-child")
+// console.log(item);
 
-let item2 = document.querySelector("ul#myList>li:first-child").innerHTML
-console.log(item2);
+// let item2 = document.querySelector("ul#myList>li:first-child").innerHTML
+// console.log(item2);
 
-document.querySelector("ul#myList>li:first-child").innerHTML = "Artik Bursa"
+// document.querySelector("ul#myList>li:first-child").innerHTML = "Artik Bursa"
 
-let ulDOM = document.querySelector("ul#myList")
-let liDOM = document.createElement('li')
+// let ulDOM = document.querySelector("ul#myList")
+// let liDOM = document.createElement('li')
 
-console.log(ulDOM);
+// console.log(ulDOM);
 
-liDOM.innerHTML = "self created element"
+// liDOM.innerHTML = "self created element"
 
-ulDOM.append(liDOM)
+// ulDOM.append(liDOM)
 
 // console.log(ulDOM.lastChild);
 
 // ulDOM.prepend(prompt("Please enter name: "))
-ulDOM.append(prompt("Please enter name: ")) //todolist muhabbeti
+// ulDOM.append(prompt("Please enter name: ")) 
+//todolist muhabbeti
+
+
+let greeting = document.querySelector("#greeting")
+
+greeting.classList.add("text-primary")
+greeting.classList.add("title")
+greeting.classList.add("new-info", "second-class", "third-class")
+
+greeting.classList.remove("new-info")
+
+
+console.log(greeting.classList);
+
 
