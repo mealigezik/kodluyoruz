ff75ea2afbf5208c4f3dceeb8f35cec6b718fbde
Ali Gezik <bilgepanter@gmail.com>
2023-11-16 20:39:44 +0100

    q

--- javascript/lesson1/if-else.js
+++ javascript/lesson1/if-else.js
@@ -1,4 +1,4 @@
-console.log("a");
+// console.log("a");
 // let username = prompt("enter your name: ")
 
 // if (username.length > 0) {
@@ -17,5 +17,23 @@ console.log("a");
 
 
 // let n = parseInt(readLine().trim(), 10);
-let n = prompt()
-n %2 ? console.log("Weird") : console.log("Not Weird");
+// let n = prompt()
+// n %2 ? console.log("Weird") : console.log("Not Weird");
+
+let userName = prompt("User Name")
+let age = prompt("Age")
+let info = document.querySelector("#info")
+
+if (userName && age >= 18){
+    info.innerHTML = "For " + userName + " Drive License is Available"
+}else if (!userName){
+    info.innerHTML = "Enter your user name"
+}else if (!age){
+    info.innerHTML = "Enter age"
+}else if (!(age >= 18)){
+    info.innerHTML = "Your age must be 18 or greater"
+}else{
+    info.innerHTML = "Don't permission"
+}
+
+console.log(info);
