2e6c683a0785947da6dd3990734eb834a7918d73
Ali Gezik <bilgepanter@gmail.com>
2023-11-16 03:08:37 +0100

    q

--- javascript/lesson1/app4.js
+++ javascript/lesson1/app4.js
@@ -1,6 +1,8 @@
 let price = "100"
+let user = "bilge"
 
 console.log("=", price == 1);
+console.log("new",price = 100);
 console.log("==",price == 100);
 console.log("===", price === 100);
 console.log("!= 2", price != 2);
@@ -11,3 +13,12 @@ console.log("price <=100", price <= 100);
 console.log("price >=100", price >= 100);
 console.log("price >100", price > 100);
 console.log("price >1", price > 1);
+
+console.log(user);
+console.log(user == "bilge");
+console.log(user != "bilge");
+
+console.log(price && user != "bilge");
+console.log(price || user != "guest");
+
+console.log(!!2)
