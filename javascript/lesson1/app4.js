8b0de45e3a24c423e076b4dc5cd2370ceaccc5c2
Ali Gezik <bilgepanter@gmail.com>
2023-11-16 02:28:26 +0100

    q

---
+++ javascript/lesson1/app4.js
@@ -0,0 +1,13 @@
+let price = "100"
+
+console.log("=", price == 1);
+console.log("==",price == 100);
+console.log("===", price === 100);
+console.log("!= 2", price != 2);
+console.log("!= 100", price != 100);
+console.log("!== 100", price !== 100);
+
+console.log("price <=100", price <= 100);
+console.log("price >=100", price >= 100);
+console.log("price >100", price > 100);
+console.log("price >1", price > 1);
