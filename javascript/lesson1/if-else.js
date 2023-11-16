b69cc17271d9cac6ab6fcfaafa8b6a0bc2602a48
Ali Gezik <bilgepanter@gmail.com>
2023-11-16 03:58:21 +0100

    q

---
+++ javascript/lesson1/if-else.js
@@ -0,0 +1,21 @@
+console.log("a");
+// let username = prompt("enter your name: ")
+
+// if (username.length > 0) {
+//     console.log(`Your user name: ${username}`)
+//     console.log("Your user name: " + username)
+//     console.log(username);
+// }
+// else {
+//     alert("Please enter a username")
+// }
+
+
+// username.length > 0 
+//         ? console.log("yes, your username is " + username) 
+//         : console.log("No");
+
+
+// let n = parseInt(readLine().trim(), 10);
+let n = prompt()
+n %2 ? console.log("Weird") : console.log("Not Weird");
