cae90162f4b5864d8b2c24b9a78c02ee791bd3b6
Ali Gezik <bilgepanter@gmail.com>
2023-11-17 10:23:48 +0100

    q

--- javascript/lesson1/functions.js
+++ javascript/lesson1/functions.js
@@ -1,3 +1,26 @@
+
 function hello(){
     console.log("Hello sir!")
+    helloWorld()
+    userCheck()
 }
+
+function helloWorld(){
+    console.log("Hello world!");
+}
+
+function userCheck(){
+    
+    let userName = prompt("Enter your Name: ");
+    let age = prompt("Enter your age: ");
+    if (userName && age >= 18){
+        info.innerHTML = "ehliyet alabilirsiniz"
+    }else if (!userName){
+        info.innerHTML = "kullanici adi giriniz"
+    }else if ( !(age >= 18)){
+        info.innerHTML = "Yas bilginiz yok yada 18 yasindan kucuksunuz"
+    }
+}
+
+
+hello()
