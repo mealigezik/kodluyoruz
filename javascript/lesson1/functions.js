4503ce80c5d1451b33651ff962934dfbcbf697e5
Ali Gezik <bilgepanter@gmail.com>
2023-11-18 00:11:35 +0100

    q

--- javascript/lesson1/functions.js
+++ javascript/lesson1/functions.js
@@ -1,44 +1,118 @@
 
-function hello() {
-    console.log("Hello sir!")
-    helloWorld()
-    userCheck()
-    // aaa()
-    // printHello()
-}
+// function hello() {
+//     console.log("Hello sir!")
+//     helloWorld()
+//     userCheck()
+//     // aaa()
+//     // printHello()
+// }
 
-function helloWorld() {
-    console.log("Hello world!");
-}
+// function helloWorld() {
+//     console.log("Hello world!");
+// }
 
-function userCheck() {
-    let userName = prompt("Enter your Name: ");
-    let age = prompt("Enter your age: ");
-    if (userName && age >= 18) {
-        info.innerHTML = "ehliyet alabilirsiniz"
-    } else if (!userName) {
-        info.innerHTML = "kullanici adi giriniz"
-    } else if (!(age >= 18)) {
-        info.innerHTML = "Yas bilginiz yok yada 18 yasindan kucuksunuz"
-    }
-}
+// function userCheck() {
+//     let userName = prompt("Enter your Name: ");
+//     let age = prompt("Enter your age: ");
+//     if (userName && age >= 18) {
+//         info.innerHTML = "ehliyet alabilirsiniz"
+//     } else if (!userName) {
+//         info.innerHTML = "kullanici adi giriniz"
+//     } else if (!(age >= 18)) {
+//         info.innerHTML = "Yas bilginiz yok yada 18 yasindan kucuksunuz"
+//     }
+// }
 
 
-hello()
+// hello()
 
-function printHello(name) {
-    info.innerHTML = `Hello ${name}`
-}
+// function printHello(name) {
+//     info.innerHTML = `Hello ${name}`
+// }
 
 // printHello("bilge")
 
-function aaa() {
-    let num1 = parseInt(prompt("first num"))
-    let num2 = parseInt(prompt("second num"))
-    function addition(num1, num2) {
-        info.innerHTML = `${num1} ve ${num2} sayilarinizin toplami ${num1 + num2} `
-    }
+// function aaa() {
+//     let num1 = parseInt(prompt("first num"))
+//     let num2 = parseInt(prompt("second num"))
+//     function addition(num1, num2) {
+//         info.innerHTML = `${num1} ve ${num2} sayilarinizin toplami ${num1 + num2} `
+//     }
 
-    addition(num1, num2)
+//     addition(num1, num2)
+// }
+
+// function printScreen1() {
+//     console.log("İlk ekran çıktısı");
+//   }
+
+//   function printScreen2(callback1, callback2) {
+//     setTimeout(function () {
+//       callback1();
+//       console.log("İkinci ekran çıktısı")
+//       callback2();
+//     }, 3000);
+//   }
+
+//   function printScreen3() {
+//     console.log("Üçüncü ekran çıktısı");
+//   }
+
+//todo----------------------------------------------------------------
+
+//  printScreen2(printScreen1, printScreen3);
+
+//  const karesiniAl = (sayi) => sayi * sayi;
+
+//  console.log(karesiniAl(3)); 
+
+//todo----------------------------------------------------------------
+
+//  let experience = prompt('Kac yillik gelistirici tecrubeniz var', 7);
+
+// const developer =
+//   experience < 5
+//     ? () => alert('Bir cok konuyu biliyorum')
+//     : () => alert('Hicbir sey bilmiyorum:)');
+
+// developer();
+
+// let toplam = (a, b) => {
+//     let result = a + b;
+//     return result;
+//   };
+
+//   console.log(toplam(7,2));
+// let x = prompt("please enter a number; ");
+// let n = prompt("query");
+
+
+// function pow(x, n) {
+//     let result = 1;
+
+//     for (let i = 0; i < n; i++) {
+//         result *= x;
+//     }
+//     return result;
+// }
+
+
+// console.log(pow(x, n));
+
+//Deneyip sonuçları görebilirsiniz.
+{
+    let message = 'Merhaba';
+    console.log(message);
+}
+// console.log(message); // ??
+//Deneyip sonuçları görebilirsiniz.
+{
+    let message = 'Merhaba';
+    console.log(message);
 }
 
+{
+    let message = 'Hello';
+    console.log(message);
+}
+// Kod blokları kullanmadan da deneyebilirsiniz.
