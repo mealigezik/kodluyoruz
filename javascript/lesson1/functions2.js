1bc55be36bdd833f692ade0d87e6c39838d5e5b1
Ali Gezik <bilgepanter@gmail.com>
2023-11-18 23:01:09 +0100

    q

---
+++ javascript/lesson1/functions2.js
@@ -0,0 +1,8 @@
+// let firstName = "Lorem"
+
+function greeting(firstName="") {
+    // console.log(`Hello  ${firstName ? firstName : "Empty"}`);
+    console.log(`Hello ${firstName}`);
+}
+
+greeting("Bilge")
