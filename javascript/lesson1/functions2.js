b7dad0f07119772b349466d119292180eba1dd0a
Ali Gezik <bilgepanter@gmail.com>
2023-11-19 00:29:07 +0100

    q

--- javascript/lesson1/functions2.js
+++ javascript/lesson1/functions2.js
@@ -8,8 +8,59 @@ function greeting(firstName="", lastName="") {
     console.log(firstName, lastName);
 }
 
-
-greeting()
-
+// greeting("Parameter", "second parameter")
 
 
+function greetings2(firstName, lastName){
+    let info = `Merhaba ${firstName} and ${lastName}`
+    return info
+}
+
+let info = greetings2("ad", "soyad")
+
+console.log(info);
+
+function domIdWriteInfo(id, info){
+    let domObject = document.querySelector(`#${id}`)
+    domObject.innerHTML = info
+}
+
+let htmlInfo = `<span style="color:red">H3</span>`
+
+domIdWriteInfo("greeting", "H1")
+domIdWriteInfo('infoSecond', greetings2("H2", "h2"))
+domIdWriteInfo('infor', htmlInfo)
+
+
+//!----------------------------------------------------------------
+
+function Question(hobby) {
+    switch (hobby) {
+      case "car":
+        return function (name) {
+          console.log(name + " do you have a car ?");
+        };
+        break;
+  
+      case "book":
+        return function (name) {
+          console.log(name + " what is your favorite author?");
+        };
+        break;
+  
+      case "software":
+        return function (name, type) {
+          console.log(name + " are you interested in " + type + "?");
+        };
+        break;
+  
+      default:
+        return function (name) {
+          console.log(name + "  how are you ?");
+        };
+        break;
+    }
+  }
+  
+  var softwareQuestion = Question("software");
+  softwareQuestion("Cemre","nodejs");
