9f6ec92c7f285a0a15b18fa9e3bbafd1f3820ece
Ali Gezik <bilgepanter@gmail.com>
2023-11-23 02:22:29 +0100

    q

--- javascript/lesson1/functions3.js
+++ javascript/lesson1/functions3.js
@@ -1,22 +1,50 @@
-function hello(firstName) {
-    console.log(`Merhaba ${firstName}`);
+// function hello(firstName) {
+//     console.log(`Merhaba ${firstName}`);
+// }
+
+// hello("Javascript")
+// // hello()
+
+// const helloFuncV1 = (firstName) => { console.log(`Merhaba ${firstName}`) }
+// helloFuncV1('Arrow Function v1')
+
+// const helloFuncV2 = firstName => console.log(`Merhaba ${firstName}`);
+// helloFuncV2('Arrow Function v2')
+
+// const helloFuncV3 = (firstName, lastName) => console.log(`Merhaba ${firstName} ${lastName}`);
+// helloFuncV3('Arrow Function v3', 'Second Element')
+
+// const helloFuncV4 = (firstName, lastName) => {
+//     let info = `Merhaba ${firstName} ${lastName}`
+//     console.log(info);
+//     return info
+// }
+// helloFuncV4('Arrow Function v3', 'Third Element')
+
+//! EVENTSLAR ----------------------------------------------------------
+
+let greeting = document.querySelector('#greeting');
+greeting.addEventListener("mouseover", domClick)
+
+function domClick(){
+
+        console.log("Tiklandi")
+        // console.log(this.innerHTML ="tiklandigi icin bilgi degisti");
+        // console.log(this)
+        // console.log(this.style.color = "red");
+        this.style.color == "red" ? this.style.color = "white" : this.style.color = "red"
+    
 }
+// let greeting = document.querySelector('#greeting');
+// greeting.addEventListener("click", domClick)
 
-hello("Javascript")
-// hello()
+// function domClick(){
 
-const helloFuncV1 = (firstName) => { console.log(`Merhaba ${firstName}`) }
-helloFuncV1('Arrow Function v1')
+//         console.log("Tiklandi")
+//         // console.log(this.innerHTML ="tiklandigi icin bilgi degisti");
+//         // console.log(this)
+//         // console.log(this.style.color = "red");
+//         this.style.color == "red" ? this.style.color = "white" : this.style.color = "red"
+    
+// }
 
-const helloFuncV2 = firstName => console.log(`Merhaba ${firstName}`);
-helloFuncV2('Arrow Function v2')
-
-const helloFuncV3 = (firstName, lastName) => console.log(`Merhaba ${firstName} ${lastName}`);
-helloFuncV3('Arrow Function v3', 'Second Element')
-
-const helloFuncV4 = (firstName, lastName) => {
-    let info = `Merhaba ${firstName} ${lastName}`
-    console.log(info);
-    return info
-}
-helloFuncV4('Arrow Function v3', 'Third Element')
