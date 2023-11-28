c312e80be3e1b6740ac1359b425ac74a0ad48554
Ali Gezik <bilgepanter@gmail.com>
2023-11-28 11:42:50 +0100

    q

---
+++ javascript/lesson1/jsUbungen.js
@@ -0,0 +1,30 @@
+//! -------SAYAC ARTIRMA-------------
+
+let counter = 0;
+let counterDOM = document.querySelector("#counter");
+let increaseDOM = document.querySelector("#increase");
+let decreaseDOM = document.querySelector("#decrease");
+
+counterDOM.innerHTML = counter
+
+increaseDOM.addEventListener("click", clickEvent)
+decreaseDOM.addEventListener("click", clickEvent)
+
+// function clickEvent() {
+//     console.log(this.id);
+//     if (this.id == "increase"){
+//         counterDOM.innerHTML = counter ++ 
+//     }else if (this.id == "decrease"){
+//         counterDOM.innerHTML = counter --
+//     }
+//     return counter
+// }
+
+// function clickEvent() {
+//     this.id == "increase" ? counterDOM.innerHTML = counter++ : counterDOM.innerHTML = counter--
+// }
+
+function clickEvent() {
+    counterDOM.innerHTML = counter
+    this.id == "increase" ? counter++ : counter--
+}
