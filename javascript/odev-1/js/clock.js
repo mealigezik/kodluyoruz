f12cda5392444fa31db98f69aa991fa1ae1ffedb
Ali Gezik <bilgepanter@gmail.com>
2023-12-01 01:06:10 +0100

    q

--- javascript/odev-1/js/clock.js
+++ javascript/odev-1/js/clock.js
@@ -1,3 +1,24 @@
-let userName = prompt("Your Name?");
+// let userName = prompt("Your Name?");
 
-document.getElementById("myName").textContent = userName;
+// document.getElementById("myName").textContent = userName;
+
+function myTime() {
+    let actualTime = document.getElementById("myClock")
+    let fullTime = new Date();
+    let hours = fullTime.getHours();
+    let minutes = fullTime.getMinutes();
+    let seconds = fullTime.getSeconds();
+    let milliseconds = fullTime.getMilliseconds();
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    milliseconds = (milliseconds < 10) ? "0" + milliseconds : milliseconds;
+
+    let screenTime = hours + ":" + minutes + ":" + seconds + ":" + Math.floor(milliseconds / 100);
+
+    actualTime.innerHTML = screenTime;
+}
+setInterval(myTime, 100)
+
+myTime();
