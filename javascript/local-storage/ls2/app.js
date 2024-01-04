fb767609a3aec5d5aed7943d81fee7fead48b60f
Ali Gezik <bilgepanter@gmail.com>
2024-01-04 11:21:11 +0100

    q

--- javascript/local-storage/ls2/app.js
+++ javascript/local-storage/ls2/app.js
@@ -1,5 +1,5 @@
 let user ={userName: "bilgepanter", isActive: true}
-console.log(user);
 localStorage.setItem("userInfo", JSON.stringify(user));
 
 let userInfo = localStorage.getItem("userInfo");
+console.log(userInfo);
